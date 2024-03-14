cb8cc4a83e6598c3dfda979d6ab5a3f1d468b813
THeAnh015 <163363370+THeAnh015@users.noreply.github.com>
2024-03-14 08:05:20 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("MP3 Player", "Portable music player", "1234", 49.99f, 180);
+        Book book = new Book("Java Programming", "Learn Java programming", "5678", 29.99f, "John Doe", 400, "Programming");
 
-        MP3 mp3;
         mp3.showInfo();
-
-        Book book;
         book.showInfo();
-
     }
 }
