ee35cbe6c899243be1fdcd7586b29a4e1ecabdea
THeAnh015 <163363370+THeAnh015@users.noreply.github.com>
2024-03-14 07:52:53 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,13 @@
-public class MP3 {
-}
+  class MP3 extends Item{
+    private int duration;
+     public M3P (String name, String description, String ID, float price, int duration){
+       super(name, description, ID, price);
+       this.duration = duration;
+     }
+
+    void showInfo()
+    {
+      System.out.println("M3P - Name: "+ name + ", Description:" + getDescription)
+    }
+    
+    }
